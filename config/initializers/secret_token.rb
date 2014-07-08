860c79938abb18bfe9ad4577c8ae1eef92515145
dexter <mostafa.hussein91@gmail.com>
2014-07-08 12:41:39 +0200

    Using Nginx and Unicorn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeployWithNginx::Application.config.secret_token = 'bad4005b44339e3d8c4cd1849faded03a8fb8e8432e55beeb34dfebc55cb7d4fdaa1aae2d5a77e51597599bf486479de084d8377f24c47cb5fbdb2acc1a25163'
